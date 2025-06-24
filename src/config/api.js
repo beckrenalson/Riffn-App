07537db65cc82307ed8b2bbad6d56b1f7821947a
beckrenalson <beck.take2@gmail.com>
2025-06-24 14:59:18 +1200

    Switch back to deployed api

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:5000";
-// import.meta.env.VITE_RIFFN_API || 
+export const API_URL = import.meta.env.VITE_RIFFN_API || "http://localhost:5000";
+
 
 export const USERS_ENDPOINT = `${API_URL}/api/users`;
 export const INSTRUMENTS_ENDPOINT = `${API_URL}/api/instruments`;
