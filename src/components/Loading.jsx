b2d4df6ffc93ba4037c9c09fd28afa6912260723
beckrenalson <beck.take2@gmail.com>
2025-06-27 09:05:27 +1200

    Change loading spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,9 @@
-import { ClipLoader } from "react-spinners";
+import { MoonLoader } from "react-spinners";
 
 function Loading() {
     return (
         <div className="flex justify-center items-center h-screen">
-            <ClipLoader color="#FFFFFF" size={60} />
+            <MoonLoader color="#FFFFFF" size={60} />
         </div>
     );
 }
