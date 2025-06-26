39dca8380472403a89d4fd9c3b1561723b2e0d47
beckrenalson <beck.take2@gmail.com>
2025-06-26 15:54:57 +1200

    Style navbar with text and blured background

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,19 +16,24 @@ function NavBar() {
 
     return (
         <>
-            <nav className="flex justify-around fixed bottom-0 w-full border p-2 bg-white">
+            <nav className="fixed bottom-0 w-full bg-[#121212]/80 backdrop-blur border-t border-gray-800 px-6 py-3 flex justify-around items-center z-50">
+
                 <button
                     onClick={search}
-                    className="w-6">
-                    <img src="/images/search.png" />
+                    className="flex flex-col items-center text-gray-400 hover:text-white transition">
+                    <img src="/images/search.png" alt="Search" className="w-6 h-6 mb-1" style={{ filter: 'invert(1)' }} />
+                    <span className="text-xs">Search</span>
                 </button>
+
                 <button
                     onClick={() => navigate("/profile")}
-                    className="w-6">
-                    <img src="/images/circle-user.png" />
+                    className="flex flex-col items-center text-gray-400 hover:text-white transition">
+                    <img src="/images/circle-user.png" alt="Profile" className="w-6 h-6 mb-1" style={{ filter: 'invert(1)' }} />
+                    <span className="text-xs">Profile</span>
                 </button>
             </nav>
         </>
+
     )
 }
 
