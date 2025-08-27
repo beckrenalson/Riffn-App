9509c85816f048a94274707796de097d620a297c
beckrenalson <beck.take2@gmail.com>
2025-08-27 13:07:15 +1200

    Fix inst and genre clear all btn not working

--- src/stores/GenreStore.jsx
+++ src/stores/GenreStore.jsx
@@ -1,12 +1,19 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import UserStore from "./UserStore";
 
 const GenreStore = create(persist(
     (set) => ({
         genreList: [],
         selectedGenres: [],
         setSelectedGenres: (genres) => set({ selectedGenres: genres }),
-        clearSelectedGenres: () => set({ selectedGenres: [] }),
+        clearSelectedGenres: () => {
+            set({ selectedGenres: [] });
+            UserStore.getState().setUserData({
+                ...UserStore.getState().userData,
+                selectedGenres: [],
+            });
+        },
         setGenreList: (list) => set({ genreList: list }),
     }),
     {
--- src/stores/InstrumentStore.jsx
+++ src/stores/InstrumentStore.jsx
@@ -1,12 +1,19 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import UserStore from "./UserStore";
 
 const InstrumentStore = create(persist(
     (set) => ({
         instrumentList: [],
         selectedInstruments: [],
         setSelectedInstruments: (instruments) => set({ selectedInstruments: instruments }),
-        clearSelectedInstruments: () => set({ selectedInstruments: [] }),
+        clearSelectedInstruments: () => {
+            set({ selectedInstruments: [] });
+            UserStore.getState().setUserData({
+                ...UserStore.getState().userData,
+                selectedInstruments: [],
+            });
+        },
         setInstrumentList: (list) => set({ instrumentList: list }),
     }),
     {
