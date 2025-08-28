9d50f577ee17b47512294dc2e0203a60068cf144
beckrenalson <beck.take2@gmail.com>
2025-08-28 15:41:51 +1200

    Check for isEditing to prevent redirection to search

--- src/components/CreateProfile/SignUpLayout.jsx
+++ src/components/CreateProfile/SignUpLayout.jsx
@@ -4,14 +4,14 @@ import UserStore from '../../../src/stores/UserStore';
 
 function SignUpLayout() {
   const navigate = useNavigate();
-  const { userData, _hasHydrated } = UserStore();
+  const { userData, _hasHydrated, isEditing } = UserStore();
 
   useEffect(() => {
-    if (_hasHydrated && userData && userData._id) {
+    if (_hasHydrated && userData && userData._id && !isEditing) {
       const target = userData.profileType === "solo" ? "band" : "solo";
       navigate(`/search/${target}`);
     }
-  }, [userData, _hasHydrated, navigate]);
+  }, [userData, _hasHydrated, navigate, isEditing]);
 
   return (
     <>
